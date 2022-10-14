155e401caa72b1f89482bfdeac42bc9ae4f91b4b
kkeeth <zensin0082@gmail.com>
2022-10-14 17:41:31 +0900

    feat: build 3.md

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 # 全部ビルド
 cp slides/1.md ./ && slidev build -o dist/1 --base /1/ 1.md && rm 1.md
 cp slides/2.md ./ && slidev build -o dist/2 --base /2/ 2.md && rm 2.md
+cp slides/3.md ./ && slidev build -o dist/3 --base /3/ 3.md && rm 3.md
 
 cp index.html dist/index.html
